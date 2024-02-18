8d12ceb3d3d3df64b141900db1462c3b61dc335e
ITboxer <46433251+ITboxer@users.noreply.github.com>
2024-02-18 14:34:56 +0900

    Create anti-tampering.h

---
+++ anti-tampering.h
@@ -0,0 +1,14 @@
+#ifndef ANTI_DEBUGGING_H
+#define ANTI_DEBUGGING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int DetectDebugger();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ANTI_DEBUGGING_H
